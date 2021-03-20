41a795b97db721f28b9662c4cdb3d67d6c2f07ef
anupvhumi <anup12.m@gmail.com>
2021-03-20 22:47:41 +0600

    dashboard invest chart implement

--- script/dashboard.php
+++ script/dashboard.php
@@ -40,7 +40,43 @@ $RecoredSet = $Database->Query("
 
 
 ");
-// DebugDump($RecoredSet[2]);
+
+$InvestRecoredSet = $Database->Query("
+                        SELECT 			COUNT(0) AS PaidCount,
+                                                SUM(ISS.InvestSchemeSettingsPayPerInstallment) AS PaidTotal
+                        FROM 			      ims_investtransaction AS IT
+                              LEFT JOIN	      ims_invest AS I ON I.InvestID = IT.InvestID
+                              LEFT JOIN	      ims_investschemesettings AS ISS ON ISS.InvestSchemeSettingsID = I.InvestSchemeSettingsID
+                              LEFT JOIN	      sphp_user AS U ON U.UserID = I.UserID
+                        WHERE			      MONTH(IT.InvestTransactionPaidDate) = MONTH(CURRENT_DATE())
+                              AND			YEAR(IT.InvestTransactionPaidDate) = YEAR(CURRENT_DATE())
+                              AND			IT.InvestTransactionIsPaid = 1;
+                        
+                        SELECT 			COUNT(0) AS UnPaidCount,
+                                                SUM(ISS.InvestSchemeSettingsPayPerInstallment) AS UnPaidTotal
+                        FROM 			      ims_investtransaction AS IT
+                              LEFT JOIN	      ims_invest AS I ON I.InvestID = IT.InvestID
+                              LEFT JOIN	      ims_investschemesettings AS ISS ON ISS.InvestSchemeSettingsID = I.InvestSchemeSettingsID
+                              LEFT JOIN	      sphp_user AS U ON U.UserID = I.UserID
+                        WHERE			      MONTH(IT.InvestTransactionPaidDate) = MONTH(CURRENT_DATE())
+                              AND			YEAR(IT.InvestTransactionPaidDate) = YEAR(CURRENT_DATE())
+                              AND			IT.InvestTransactionIsPaid = 0;
+
+                        SELECT 			COUNT(0) AS TransactionCount,
+                                                IF(IT.InvestTransactionIsPaid = 1, 'PAID', 'UNPAID') AS Status
+                        FROM 			      ims_investtransaction AS IT
+                              LEFT JOIN	      ims_invest AS I ON I.InvestID = IT.InvestID
+                              LEFT JOIN	      ims_investschemesettings AS ISS ON ISS.InvestSchemeSettingsID = I.InvestSchemeSettingsID
+                              LEFT JOIN	      sphp_user AS U ON U.UserID = I.UserID
+                        WHERE			      (MONTH(IT.InvestTransactionPaidDate) = MONTH(CURRENT_DATE())
+                                    AND			YEAR(IT.InvestTransactionPaidDate) = YEAR(CURRENT_DATE()))
+                              OR			(MONTH(IT.InvestTransactionPayableDate) = MONTH(CURRENT_DATE())
+                                    AND			YEAR(IT.InvestTransactionPayableDate) = YEAR(CURRENT_DATE()))
+                        GROUP BY			IT.InvestTransactionIsPaid
+
+
+");
+// DebugDump($InvestRecoredSet);
 
 print "<div class=\"Welcome\">
             <div class=\"Title\">Welcome {$User->Name()}!</div>
@@ -48,7 +84,7 @@ print "<div class=\"Welcome\">
 
 $Output[] = "
       <div class=\"ContainerRow\">
-
+            <h3>Lend Statistics</h3>      
             <div class=\"Container Container_3Column\">
                   <div class=\"Subtitle\">Status</div>
                   <div class=\"Content Content_Height247\"><canvas id=\"StatusChart\" class=\"Chart\"></canvas></div>
@@ -128,6 +164,89 @@ $Output[] = "
 
 print "
       " . implode(null, $Output) . "
+";
+
+$Output2[] = "
+      <div class=\"ContainerRow\">
+            <h3>Invest Statistics</h3>      
+            <div class=\"Container Container_3Column\">
+                  <div class=\"Subtitle\">Status</div>
+                  <div class=\"Content Content_Height247\"><canvas id=\"InvestChart\" class=\"Chart\"></canvas></div>
+            </div>
+
+            <div class=\"Container Container_3Column\" id=\"paid\">
+                  <div class=\"Subtitle\">Paid</div>
+                  <div class=\"Content Content_Height247\">
+                        <h3>" . $Month . "</h3>
+                        <p> Complete " . $InvestRecoredSet[0][0]['PaidCount'] . " Transactions</p>
+                        <p> Paid Amount $" . $InvestRecoredSet[0][0]['PaidTotal'] . "</p>
+                  </div>
+            </div>
+
+            <div class=\"Container Container_3Column\" id=\"unpaid\">
+                  <div class=\"Subtitle\">Due</div>
+                  <div class=\"Content Content_Height247\">
+                        <h3>" . $Month . "</h3>
+                        <p> Remain " . $InvestRecoredSet[1][0]['UnPaidCount'] . " Transactions</p>
+                        <p> Due Amount $" . $InvestRecoredSet[1][0]['UnPaidTotal'] . "</p>
+                  </div>
+            </div>
+
+      </div>
+      <script>
+      " . (isset($InvestRecoredSet[2]) ? "
+            var InvestChart = new Chart(document.getElementById('InvestChart').getContext('2d'), {
+                type: 'pie', // bar, horizontalBar, line, doughnut, pie
+                data: {
+                    labels: ['" . implode("', '", array_column($InvestRecoredSet[2], "Status")) . "'],
+                    datasets: [
+                        {
+                            backgroundColor: ['red', 'green'],
+                            data: [" . implode(", ", array_column($InvestRecoredSet[2], "TransactionCount")) . "],
+                            borderWidth: 0.5,
+                            hoverBackgroundColor: '#ff99dd',
+                        },
+                    ]
+                },
+                options: {
+                    maintainAspectRatio: false, // true, false
+                    legend: {
+                        position: 'bottom',
+                        labels: {
+                            usePointStyle: true,
+                        },
+                    },
+                    plugins: {
+                        datalabels: {
+                            formatter: (value, ctx) => {
+
+                                let datasets = ctx.chart.data.datasets;
+                    
+                                if (datasets.indexOf(ctx.dataset) === datasets.length - 1) {
+                                let sum = datasets[0].data.reduce((a, b) => a + b, 0);
+                                let percentage = Math.round((value / sum) * 100) + '%';
+                                return percentage;
+                                } else {
+                                return percentage;
+                                }
+                            },
+                            color: 'black',
+                            anchor: 'center',
+                            align: 'center',
+                            font: {
+                                weight: 'bold',
+                            },
+                        },
+                    },
+                },
+            });
+        " : null) . "
+
+		</script>
+      ";
+
+print "
+      " . implode(null, $Output2) . "
 ";
 
 ?>
